b6a537a3ab0989acbf61b9733cad20dc4229c53a
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2022-03-16 15:41:36 -0300

    Change routes to tenant login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
+//   Register route to create a tenant
+Route::middleware('guest')->group(function () {
+    Route::post('register', [RegisteredUserController::class, 'store']);
+});
