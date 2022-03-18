c556092ddf1294771753148a9480531cf079d77d
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2022-03-18 19:55:23 -0300

    Fix Layout and remove typos models name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,8 @@
     </head>
 
     <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
 
         <div class="min-h-screen bg-gray-100">
             <!-- Page Heading -->
